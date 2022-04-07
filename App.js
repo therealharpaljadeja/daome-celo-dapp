07ab45dfad6284881bcc43421af11b2555bc34b1
therealharpaljadeja <jadejaharpal14@gmail.com>
2022-04-07 21:41:05 +0530

    connect wallet working

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, View, Button, Text } from "react-native";
+import { useWalletConnect } from "@walletconnect/react-native-dapp";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const connector = useWalletConnect();
+	const [account, setAccount] = useState(null);
+	const connectWallet = React.useCallback(() => {
+		if (connector.connected) {
+			setAccount(connector.accounts[0]);
+		} else {
+			return connector.connect();
+		}
+	}, [connector]);
+
+	return (
+		<View style={styles.container}>
+			{account ? (
+				<Text>{account}</Text>
+			) : (
+				<Button
+					onPress={() => connectWallet()}
+					title='Connect Wallet'
+				/>
+			)}
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
